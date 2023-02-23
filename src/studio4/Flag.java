cb11a839c79674c94e01760e436f0ca20a872c55
hex <hex@localhost>
2023-02-23 12:06:13 -0600

    Studio04

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,11 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledCircle(.5, 0.5, 0.12);
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.rectangle(.5, 0.5, 0.3, 0.2);
+		
 		
 	}
 }
